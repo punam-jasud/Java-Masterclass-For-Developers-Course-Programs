3a912ccdf7d0053b8e844e2564e26de7f843a6f2
unknown <punamjasud98@gmail.com>
2020-08-19 19:32:39 +0530

    PrintSumAndAvarage Java File Added in Section 5

---
+++ JavaMasterClassPrograms/src/section5/PrintSumAndAvarage.java
@@ -0,0 +1,37 @@
+package section5;
+
+import java.util.Scanner;
+
+public class PrintSumAndAvarage {
+
+	public static void main(String[] args) {
+		
+		inputThenPrintSumAndAvarage();
+	}
+	public  static void inputThenPrintSumAndAvarage() {
+		Scanner scanner = new Scanner(System.in);
+		
+		int sum = 0  , count = 0;
+		long avg = 0;
+		
+		while(true)
+		{
+			System.out.println("Enter Number : ");
+			boolean isAnInt = scanner.hasNextInt();
+			
+			if(isAnInt)
+			{
+				int number = scanner.nextInt();
+				sum = sum + number;
+				count++;
+			}
+			else {
+				avg = Math.round((double) sum / count);
+	            System.out.println("SUM = " + sum + " AVG = " + avg);
+	            break;
+			}
+			scanner.nextLine();
+		}
+		scanner.close();
+	}
+}
